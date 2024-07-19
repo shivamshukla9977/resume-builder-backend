e461e3bcc258b811e2d6566de5244a6056a5a02d
shivam <skshukla9977@gmail.com>
2024-07-19 11:52:07 +0530

    Migrated all migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,68 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_19_060801) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_19_062037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "educations", force: :cascade do |t|
+    t.string "title"
+    t.string "organisation"
+    t.integer "start_year"
+    t.integer "finish_year"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_educations_on_user_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "company_name"
+    t.date "start_from"
+    t.date "leave_on"
+    t.string "type"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_links_on_user_id"
+  end
+
+  create_table "overviews", force: :cascade do |t|
+    t.string "title"
+    t.text "summary"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_overviews_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "duration"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_skills_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -26,4 +85,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_19_060801) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "educations", "users"
+  add_foreign_key "jobs", "users"
+  add_foreign_key "links", "users"
+  add_foreign_key "overviews", "users"
+  add_foreign_key "projects", "users"
+  add_foreign_key "skills", "users"
 end
