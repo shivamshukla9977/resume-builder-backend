450c998418da10c2db42e22eef73c0c5456e0fbe
shivam <skshukla9977@gmail.com>
2024-07-19 12:07:45 +0530

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users do
+    resources :educations
+    resources :skills
+    resources :links
+    resources :projects
+    resources :overviews
+    resources :jobs
+  end
 end
