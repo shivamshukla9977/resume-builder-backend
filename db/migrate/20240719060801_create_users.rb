792eab3185e87c4cdb55fed707f9f36231f05e80
shivam <skshukla9977@gmail.com>
2024-07-19 11:42:30 +0530

    Added User Model

---
+++ db/migrate/20240719060801_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+      t.string :email
+      t.string :city
+      t.string :state
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
