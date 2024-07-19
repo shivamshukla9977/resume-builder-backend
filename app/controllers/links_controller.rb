cdbbd874084c827b5253ae46bf37597544eabaf1
shivam <skshukla9977@gmail.com>
2024-07-19 12:11:29 +0530

    Define Links CRUD actions

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,53 @@
 class LinksController < ApplicationController
+  before_action :set_user
+
+  def index
+    @links = @user.links
+  end
+
+  def show
+    @link = @user.links.find(params[:id])
+  end
+
+  def new
+    @link = @user.links.build
+  end
+
+  def create
+    @link = @user.links.build(link_params)
+    if @link.save
+      redirect_to user_link_path(@user, @link)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @link = @user.links.find(params[:id])
+  end
+
+  def update
+    @link = @user.links.find(params[:id])
+    if @link.update(link_params)
+      redirect_to user_link_path(@user, @link)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @link = @user.links.find(params[:id])
+    @link.destroy
+    redirect_to user_links_path(@user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def link_params
+    params.require(:link).permit(:title, :link)
+  end
 end
