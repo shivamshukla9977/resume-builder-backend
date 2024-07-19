a05d36a46542c7fc02797ee493ff339afa1ff9f8
shivam <skshukla9977@gmail.com>
2024-07-19 12:10:19 +0530

    Define Educations CRUD actions

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,2 +1,53 @@
 class EducationsController < ApplicationController
+  before_action :set_user
+
+  def index
+    @educations = @user.educations
+  end
+
+  def show
+    @education = @user.educations.find(params[:id])
+  end
+
+  def new
+    @education = @user.educations.build
+  end
+
+  def create
+    @education = @user.educations.build(education_params)
+    if @education.save
+      redirect_to user_education_path(@user, @education)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @education = @user.educations.find(params[:id])
+  end
+
+  def update
+    @education = @user.educations.find(params[:id])
+    if @education.update(education_params)
+      redirect_to user_education_path(@user, @education)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @education = @user.educations.find(params[:id])
+    @education.destroy
+    redirect_to user_educations_path(@user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def education_params
+    params.require(:education).permit(:title, :organisation, :start_year, :finish_year)
+  end
 end
